e4bdd4f365cc2219be3f5f10731fed36067bb4f7
Ben Reynwar <ben@reynwar.net>
2015-04-22 17:40:23 -0700

    Adding simple_module as example.

--- hdl/test/qa_simple_module.py
+++ hdl/test/qa_simple_module.py
@@ -6,19 +6,22 @@ import random
 import testfixtures
 
 from pyvivado import config, project
-from pyvivado.hdl.test import testA
+from pyvivado.hdl.test import simple_module
 
 logger = logging.getLogger(__name__)
 
-class TestTestA(unittest.TestCase):
+class TestSimpleModule(unittest.TestCase):
 
-    def test_one(self):
-        directory = os.path.abspath('proj_test_testA')
+    def test_pass_through(self):
+        '''
+        Tests that the inputs are passing straight through SimpleModule
+        as expected.
+        '''
+
+        directory = os.path.abspath('proj_testsimplemodule')
         data_width = 4
-        array_length = 6
         params = {
             'data_width': data_width,
-            'array_length': array_length,
         }
 
         # Make input and expected data
@@ -30,35 +33,25 @@ class TestTestA(unittest.TestCase):
             input_d = {
                 'i_valid': random.randint(0, 1),
                 'i_data': random.randint(0, max_data),
-                'i_array': [random.randint(0, max_data)
-                            for i in range(array_length)],
             }
             expected_d = {
                 'o_valid': input_d['i_valid'],
                 'o_data': input_d['i_data'],
-                'o_array': input_d['i_array'],
             }
             input_data.append(input_d)
             expected_data.append(expected_d)
 
         # Create project
-        interface = testA.get_testA_interface(params)
+        interface = simple_module.get_simple_module_interface(params)
         p = project.FileTestBenchProject.create_or_update(
             interface=interface,
             directory=directory,
-            board=config.default_board,
-            part=config.default_part,
         )
-        t = p.wait_for_most_recent_task()
-        errors = t.get_errors()
-        self.assertEqual(len(errors), 0)
 
         # Run the simulation
-        runtime = '{} ns'.format((len(input_data) + 20) * 10)
-        errors, output_data = p.run_simulation(
-            input_data=input_data, runtime=runtime)
-        self.check_output(output_data[1:], expected_data)
+        errors, output_data = p.run_simulation(input_data)
         self.assertEqual(len(errors), 0)
+        self.check_output(output_data[1:], expected_data)
 
     def check_output(self, output_data, expected_data):
         self.assertTrue(len(output_data) >= len(expected_data))
@@ -67,5 +60,5 @@ class TestTestA(unittest.TestCase):
         
         
 if __name__ == '__main__':
-    config.setup_logging(logging.DEBUG)
+    config.setup_logging(logging.WARNING)
     unittest.main()
---
+++ hdl/test/simple_module.py
@@ -0,0 +1,48 @@
+import os
+import logging
+
+from pyvivado import interface, signal, config, builder
+
+logger = logging.getLogger(__name__)
+
+class SimpleModuleBuilder(builder.Builder):
+    '''
+    Responsible for creating (or specifying) the necessary HDL files
+    and knowing with IP is required.
+
+    Since this is a very simple module all the builder does is specify the
+    VHDL file that is used.
+    '''
+    
+    def __init__(self, params):
+        super().__init__(params)
+        self.simple_filenames = [
+            os.path.join(config.hdldir, 'test', 'simple_module.vhd'),
+        ]
+        
+def get_simple_module_interface(params):
+    '''
+    Creates an interface object that is used to generate the verification
+    wrappers.
+    '''
+    module_name = 'SimpleModule'
+    data_width = params['data_width']
+    builder = SimpleModuleBuilder({})
+    module_parameters = {
+        'DATA_WIDTH': data_width,
+    }
+    wires_in = (
+        ('i_valid', signal.std_logic_type),
+        ('i_data', signal.StdLogicVector(width=data_width)),
+    )
+    wires_out = (
+        ('o_valid', signal.std_logic_type),
+        ('o_data', signal.StdLogicVector(width=data_width)),
+    )
+    iface = interface.Interface(
+        wires_in, wires_out, module_name=module_name,
+        parameters=params, module_parameters=module_parameters,
+        builder=builder)
+    return iface
+
+interface.add_to_module_register('SimpleModule',  get_simple_module_interface)
